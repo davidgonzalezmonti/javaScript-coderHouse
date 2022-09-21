09e6045a2b902256ce9e80fb48ef855080e84645
David Gonzalez Monti <107081679+davidgonzalezmonti@users.noreply.github.com>
2022-09-21 16:52:15 -0300

    segunda-entrega-final

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 //TODO STOCK DE STICKERS
-  const stockStickers = [
+const stockStickers = [
 
-    { id: 1, cantidad: 1, nombre: "Mugiwara", tamaño: "10x10", precio: 200, imagen: "./img/mugiwara.jpg", anime: "One Piece" },
-    { id: 2, cantidad: 1, nombre: "Luffy", tamaño: "8x8", precio: 150, imagen: "./img/luffy.png", anime: "One Piece" },
-    { id: 3, cantidad: 1, nombre: "Zoro", tamaño: "8x8", precio: 150, imagen: "./img/zoro.jpg", anime: "One Piece" },
-    { id: 4, cantidad: 1, nombre: "Goku", tamaño: "8x8", precio: 150, imagen: "./img/goku.png", anime: "Dragon Ball Z" },
-    { id: 5, cantidad: 1, nombre: "Vegeta", tamaño: "6x6", precio: 120, imagen: "./img/vegeta.png", anime: "Dragon Ball Z" },
-    { id: 6, cantidad: 1, nombre: "Kid Buu", tamaño: "6x6", precio: 120, imagen: "./img/boo.jpg", anime: "Dragon Ball Z" },
-    { id: 7, cantidad: 1, nombre: "Tanjiro", tamaño: "8x8", precio: 150, imagen: "./img/tanjiro.png", anime: "Kimetsu no Yaiba" },
-    { id: 8, cantidad: 1, nombre: "Inosuke", tamaño: "6x6", precio: 120, imagen: "./img/inosuke.png", anime: "Kimetsu no Yaiba" },
-    { id: 9, cantidad: 1, nombre: "Zenitsu", tamaño: "6x6", precio: 120, imagen: "./img/zenitsu.png", anime: "Kimetsu no Yaiba" },
-    
-    ];
+  { id: 1, cantidad: 1, nombre: "Mugiwara", tamaño: "10x10", precio: 200, imagen: "./img/mugiwara.jpg", anime: "One Piece" },
+  { id: 2, cantidad: 1, nombre: "Luffy", tamaño: "8x8", precio: 150, imagen: "./img/luffy.png", anime: "One Piece" },
+  { id: 3, cantidad: 1, nombre: "Zoro", tamaño: "8x8", precio: 150, imagen: "./img/zoro.jpg", anime: "One Piece" },
+  { id: 4, cantidad: 1, nombre: "Goku", tamaño: "8x8", precio: 150, imagen: "./img/goku.png", anime: "Dragon Ball Z" },
+  { id: 5, cantidad: 1, nombre: "Vegeta", tamaño: "6x6", precio: 120, imagen: "./img/vegeta.png", anime: "Dragon Ball Z" },
+  { id: 6, cantidad: 1, nombre: "Kid Buu", tamaño: "6x6", precio: 120, imagen: "./img/boo.jpg", anime: "Dragon Ball Z" },
+  { id: 7, cantidad: 1, nombre: "Tanjiro", tamaño: "8x8", precio: 150, imagen: "./img/tanjiro.png", anime: "Kimetsu no Yaiba" },
+  { id: 8, cantidad: 1, nombre: "Inosuke", tamaño: "6x6", precio: 120, imagen: "./img/inosuke.png", anime: "Kimetsu no Yaiba" },
+  { id: 9, cantidad: 1, nombre: "Zenitsu", tamaño: "6x6", precio: 120, imagen: "./img/zenitsu.png", anime: "Kimetsu no Yaiba" },
+
+];
 
 //! FIN DE STOCK
 
@@ -19,18 +19,27 @@
 let carrito = []
 
 
-//* ID llamados desde el HTML existente
-    const contenedorDeStickers = document.getElementById("contenedorStickers")
-    const tablaCarrito = document.getElementById("tablaCarrito")
-    const vaciarCarrito = document.getElementById("vaciarCarrito")
+//* ID llamados desde el HTML
+const contenedorDeStickers = document.getElementById("contenedorStickers")
+const tablaCarrito = document.getElementById("tablaCarrito")
+const vaciarCarrito = document.getElementById("vaciarCarrito")
+const formBuscador = document.getElementById("formBuscador")
 
-//* INSERTAMOS LOS PRODUCTOS EN EL HTML 
-//*por cada una
+//* LocalStorage
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("carrito")) {
+    carrito = JSON.parse(localStorage.getItem("carrito"))
+    visualizarCarrito()
+  }
+})
+
+//* Insertamos el stock en el HTML 
 stockStickers.forEach(sticker => {
-    const divSticker = document.createElement("div"); 
-    divSticker.classList.add("card", "m-4", "p-3", "mb-5", "bg-body","rounded", "diseñoCard");
-    divSticker.style = "width: 18rem";
-    divSticker.innerHTML = `
+  const divSticker = document.createElement("div");
+  divSticker.classList.add("card", "m-4", "p-3", "mb-5", "bg-body", "rounded", "diseñoCard");
+  divSticker.style = "width: 18rem";
+  divSticker.innerHTML = `
   
    <img src="${sticker.imagen}" class="card-img-top" alt="${sticker.nombre}" />
    <div class="card-body">
@@ -44,48 +53,48 @@ stockStickers.forEach(sticker => {
    </div>
         `
 
-      contenedorDeStickers.appendChild(divSticker);
+  contenedorDeStickers.appendChild(divSticker);
 
 
-//* Boton para agregar al carrito
-      const button = document.getElementById(`agregar${sticker.id}`)
-      button.addEventListener("click", () => {
-        agregarAlCarrito(sticker.id)
-      })
-      
-    
-    })
-
-
-//* Recorrido para agregar cada producto al carrito
-    const agregarAlCarrito = (stickerId) => {
-      const existe = carrito.some (prod => prod.id === stickerId)
-
-if (existe) {
-  const sticker = carrito.map (prod => {
-    if (prod.id === stickerId) {
-      prod.cantidad++
-    } 
+  //* Boton para agregar al carrito
+  const button = document.getElementById(`agregar${sticker.id}`)
+  button.addEventListener("click", () => {
+    agregarAlCarrito(sticker.id)
   })
-} else {
-  const sticker = stockStickers.find ((prod) => prod.id === stickerId)
-      carrito.push(sticker)
+
+
+})
+
+
+//* Funcion para agregar cada producto al carrito
+const agregarAlCarrito = (stickerId) => {
+  const existe = carrito.some(prod => prod.id === stickerId)
+
+  if (existe) {
+    const sticker = carrito.map(prod => {
+      if (prod.id === stickerId) {
+        prod.cantidad++
+      }
+    })
+  } else {
+    const sticker = stockStickers.find((prod) => prod.id === stickerId)
+    carrito.push(sticker)
+  }
+  visualizarCarrito()
 }
-      visualizarCarrito()
-    }
 
 
 //* Eliminar productos del carrito
 const eliminarDelCarrito = (stickerId) => {
-  const sticker = carrito.find ((prod) => prod.id === stickerId)
-const indice = carrito.indexOf(sticker)
- if (sticker.cantidad === 1) {
-  carrito.splice(indice, 1)
- } else {
- sticker.cantidad--
- }
+  const sticker = carrito.find((prod) => prod.id === stickerId)
+  const indice = carrito.indexOf(sticker)
+  if (sticker.cantidad === 1) {
+    carrito.splice(indice, 1)
+  } else {
+    sticker.cantidad--
+  }
   visualizarCarrito()
-  
+
 }
 
 
@@ -99,24 +108,26 @@ vaciarCarrito.addEventListener("click", () => {
 //* Genero un carrito con su estructura HTML
 const visualizarCarrito = () => {
   tablaCarrito.innerHTML = ""
-carrito.forEach ((sticker) => {
-const divVisualizarCarrito = document.createElement("tr")
-divVisualizarCarrito.classList.add("productoCarrito")
-divVisualizarCarrito.innerHTML = `
+  carrito.forEach((sticker) => {
+    const trVisualizarCarrito = document.createElement("tr")
+    trVisualizarCarrito.classList.add("productoCarrito")
+    trVisualizarCarrito.innerHTML = `
 <th scope="row">${sticker.id}</th>
 <td>${sticker.nombre}</td>
 <td>${sticker.tamaño}</td>
 <td>${sticker.cantidad} </td>
-<td>${sticker.precio*sticker.cantidad}</td>
+<td>${sticker.precio * sticker.cantidad}</td>
 <td><i onclick="eliminarDelCarrito(${sticker.id})" class="fas fa-trash-alt"></i></p></td>
 
 
 `
-tablaCarrito.appendChild(divVisualizarCarrito)
+    tablaCarrito.appendChild(trVisualizarCarrito)
 
-})
+    localStorage.setItem("carrito", JSON.stringify(carrito))
 
-totalCarrito()
+  })
+
+  totalCarrito()
 
 }
 
@@ -125,9 +136,9 @@ const totalCarrito = () => {
   const precioTotal = document.getElementById("precioTotal")
   carrito.forEach((sticker) => {
     const precio = Number(sticker.precio)
-    total = total + precio*sticker.cantidad
+    total = total + precio * sticker.cantidad
   })
-  
+
   precioTotal.innerHTML = `$${total}`
 }
 
@@ -137,9 +148,54 @@ botonComprar.addEventListener("click", () => {
 })
 
 
-//TODO: CREAR UN BUSCADOR PARA PODER FILTRAR POR ANIME
+//TODO: CREO UN BUSCADOR PARA PODER FILTRAR POR ANIME
 
+//* Creo la funcionalidad del boton y sus filtros
+formBuscador.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let opcionesBusquedaUI = document.getElementById("opcionesBusqueda").value;
+  console.log(opcionesBusquedaUI);
+  filtradoPorAnime(opcionesBusquedaUI)
 
+  formBuscador.reset()
+})
 
+//* Estructura HTML del filtrado
+const filtradoPorAnime = (opcionesBusqueda) => {
+  contenedorDeStickers.innerHTML = ""
+let stockStickerFiltrado;
+  if(stockStickers.find(sticker => sticker.anime === opcionesBusqueda) !== undefined){
+    stockStickerFiltrado = stockStickers.filter(sticker => sticker.anime === opcionesBusqueda)
+  } else {
+    stockStickerFiltrado = stockStickers
+  }
 
+  stockStickerFiltrado.forEach(sticker => {
+  const divSticker = document.createElement("div");
+  divSticker.classList.add("card", "m-4", "p-3", "mb-5", "bg-body", "rounded", "diseñoCard");
+  divSticker.style = "width: 18rem";
+  divSticker.innerHTML = `
+  
+   <img src="${sticker.imagen}" class="card-img-top" alt="${sticker.nombre}" />
+   <div class="card-body">
+      <h5 class="card-title">${sticker.nombre}</h5>
+      <p class="card-text">
+        Precio: <b>$${sticker.precio}</b> <br />
+        Tamaño: ${sticker.tamaño}<br />
+        Anime: ${sticker.anime}
+      </p>
+      <button href="#" id="agregar${sticker.id}" class="btn btn-primary">Agregar al carrito</button>
+   </div>
+        `
+
+  contenedorDeStickers.appendChild(divSticker);
+
+  const button = document.getElementById(`agregar${sticker.id}`)
+  button.addEventListener("click", () => {
+    agregarAlCarrito(sticker.id)
+  })
+  
+})
+
+}
 
